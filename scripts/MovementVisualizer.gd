f5ad000427289e17c6fc1e9d2fc9b55afcf66e49
GodMadeEarth <88457199+GodMadeEarth@users.noreply.github.com>
2023-08-21 05:10:54 -0400

    Added Movement Visualizer

---
+++ scripts/MovementVisualizer.gd
@@ -0,0 +1,46 @@
+class_name MovementVisualizer extends CommandComponent
+
+func _get_configuration_warnings():
+	if get_parent() is CharacterBody2D:
+		return ""
+	else:
+		return "This component depends on a CharacterBody2D parent"
+	
+	if get_parent().find_child("DirectMoveCommand"):
+		return ""
+	else:
+		return "This component depends on a DirectMoveCommand sibling"
+
+@onready var controller: MovementController = get_parent().find_child("MovementController")
+@onready var directMoveComand: DirectMoveCommand = get_parent().find_child("DirectMoveCommand")
+var ghostCharacter:Node2D
+
+func start():
+	state = CommandComponentState.RUNNING
+	ghostCharacter = Node2D.new()
+	ghostCharacter.modulate = Color(1, 1, 1, 0.314)
+	add_child(ghostCharacter)
+	
+	ghostCharacter.global_position = directMoveComand.ground_target
+	
+	for child in get_parent().get_children():
+		if child is Node2D and not child is CollisionObject2D:
+			ghostCharacter.add_child(child.duplicate(1))
+
+func stop():
+	state = CommandComponentState.INACTIVE
+	for child in get_children():
+		child.queue_free()
+
+func _run(_delta):
+	ghostCharacter.global_position = directMoveComand.ground_target
+
+func _physics_process(delta):
+	match [state,directMoveComand.state]:
+		[CommandComponentState.INACTIVE,CommandComponentState.RUNNING]:
+			start()
+		[CommandComponentState.RUNNING,CommandComponentState.INACTIVE]:
+			stop()
+		[CommandComponentState.RUNNING,CommandComponentState.RUNNING]:
+			_run(delta)
+
